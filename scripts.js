be3cd91d2bb733de702df1c29120d38def4a29a0
nenhures <xcarecollective@gmail.com>
2024-07-16 11:02:57 +0100

    First site upload
    by Rafaela

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function openMobileMenu() {
+    var x = document.getElementById("menuMobile");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+    }
